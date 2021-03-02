ddebce66e3daca9ff2cc894d533aecc3fd4da1dc
Gabriela Papp <gpgabika@gmail.com>
2021-03-02 18:53:38 +0000

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <section className='section about-section'>
+            <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime accusamus deserunt et qui veritatis iste odit pariatur molestiae repellendus doloribus. Nobis voluptatem et adipisci animi, error, atque eos ipsum voluptatibus corrupti optio accusamus illum. Magnam deleniti labore qui rem obcaecati?</h1>
+        </section>
+    )
+}
+
+export default About
